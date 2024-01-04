cffbc7a515e301257f255971961170b5ee155d88
Simon Saez <nayxxaxx789super@gmail.com>
2024-01-04 06:37:46 -0300

    Modificaciones

--- frontend-loa/src/App.js
+++ frontend-loa/src/App.js
@@ -1,4 +1,6 @@
+import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainComponent from "./components/MainComponent";
 
 function App() {
